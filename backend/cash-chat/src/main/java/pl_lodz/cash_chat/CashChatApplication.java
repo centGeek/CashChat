bac7d924a61fd07c4e39781dae97c45f690eb40a
247638 Centkowski Łukasz <247638@edu.p.lodz.pl>
2025-05-25 14:26:57 +0200

    init project

---
+++ backend/cash-chat/src/main/java/pl_lodz/cash_chat/CashChatApplication.java
@@ -0,0 +1,13 @@
+package pl_lodz.cash_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashChatApplication.class, args);
+	}
+
+}
